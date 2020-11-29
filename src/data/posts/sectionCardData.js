ef7ff01907d0b270a413838f7d2afb943e770662
Lucas Felipe <conhecimento54321@gmail.com>
2020-11-29 10:13:51 -0300

    Update posts data :art:

--- src/data/posts/sectionCardData.js
+++ src/data/posts/sectionCardData.js
@@ -2,33 +2,33 @@ const sectionCardData = {
   post1:{
     image: "https://images.pexels.com/photos/2004161/pexels-photo-2004161.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     title: "React useContext()",
-    description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+    description: "React hooks allows us to take a Reactjs functional component and add state and lifecycle methods to it. In simple words, React hooks are special functions to extend the capabilities of functional components and give them the possibility to have lifecycle events and manage state",
     avatar: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     authorName: "Guy Edward Clemons",
     date: "May 15, 2020"
   },
   post2:{
     image: "https://images.pexels.com/photos/34600/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    title: "React useContext()",
-    description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+    title: "React conditional rendering",
+    description: "Conditional rendering in React works the same way conditions work in JavaScript. Use JavaScript operators like if or the conditional operator to create elements representing the current state, and let React update the UI to match them.",
     avatar: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2734&q=80",
-    authorName: "Guy Edward Clemons",
+    authorName: "Eliza Raynold",
     date: "May 15, 2020"
   },
   post3:{
     image:"https://images.pexels.com/photos/1181263/pexels-photo-1181263.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    title: "React useContext()",
-    description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+    title: "React Lifting State Up",
+    description: "There should be a single “source of truth” for any data that changes in a React application. Usually, the state is first added to the component that needs it for rendering. Then, if other components also need it, you can lift it up to their closest common ancestor. Instead of trying to sync the state between different components, you should rely on the top-down data flow.",
     avatar: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    authorName: "Guy Edward Clemons",
+    authorName: "Alina Simons",
     date: "May 15, 2020"
   },
   post4:{
     image: "https://images.pexels.com/photos/325111/pexels-photo-325111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    title: "React useContext()",
-    description: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
+    title: "What is Material-ui",
+    description: "React components for faster and easier web development. Build your own design system, or start with Material Design.",
     avatar: "https://images.unsplash.com/photo-1584999734482-0361aecad844?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=80",
-    authorName: "Guy Edward Clemons",
+    authorName: "Luke Anthony",
     date: "May 15, 2020"
   }
 }
