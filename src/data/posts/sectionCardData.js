29b6bb72c93b0815e7168f3223b6ba25157d311c
Lucas Felipe <conhecimento54321@gmail.com>
2020-11-29 10:43:40 -0300

    Update data dates 📝

--- src/data/posts/sectionCardData.js
+++ src/data/posts/sectionCardData.js
@@ -13,7 +13,7 @@ const sectionCardData = {
     description: "Conditional rendering in React works the same way conditions work in JavaScript. Use JavaScript operators like if or the conditional operator to create elements representing the current state, and let React update the UI to match them.",
     avatar: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2734&q=80",
     authorName: "Eliza Raynold",
-    date: "May 15, 2020"
+    date: "Oct 5, 2020"
   },
   post3:{
     image:"https://images.pexels.com/photos/1181263/pexels-photo-1181263.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
@@ -21,7 +21,7 @@ const sectionCardData = {
     description: "There should be a single “source of truth” for any data that changes in a React application. Usually, the state is first added to the component that needs it for rendering. Then, if other components also need it, you can lift it up to their closest common ancestor. Instead of trying to sync the state between different components, you should rely on the top-down data flow.",
     avatar: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     authorName: "Alina Simons",
-    date: "May 15, 2020"
+    date: "May 2, 2020"
   },
   post4:{
     image: "https://images.pexels.com/photos/325111/pexels-photo-325111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
@@ -29,7 +29,7 @@ const sectionCardData = {
     description: "Material-UI is a library that provides React components for easy and fast web development. We can easily put together really aesthetic and functional components and make it work according to our use as all the components are configurable. This saves a lot of time as we don’t have to struggle with CSS to make things presentable. Material-UI components work in isolation.",
     avatar: "https://images.unsplash.com/photo-1584999734482-0361aecad844?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=80",
     authorName: "Luke Anthony",
-    date: "May 15, 2020"
+    date: "Dec 29, 2020"
   }
 }
 
