9162aa74b4beac61d2e69a3bedf234585e80ea1d
Lucas Felipe <conhecimento54321@gmail.com>
2020-11-29 08:20:04 -0300

    Remove unused files 🔥

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { ThemeProvider } from '@material-ui/core';
 import theme from './MUI theme';
-
-import './App.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Articles from './components/Articles';
